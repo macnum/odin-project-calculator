4db933ce4aa7c05db401f2e67c652fd09eb2da6d
macnum <w.damilola@yahoo.com>
2025-05-20 09:37:58 +0100

    feat: add basic calculator setup

---
+++ js/main.js
@@ -0,0 +1,18 @@
+let number1 = null;
+let number2 = null;
+let operate = null;
+
+function add(a, b) {
+	return a + b;
+}
+function subtract(a, b) {
+	return a - b;
+}
+function multiply(a, b) {
+	return a / b;
+}
+function divide(a, b) {
+	return a / b;
+}
+
+function operate(operate, number1, number2) {}
